0d89e09cf15416ff4d93a2b799ef5ecb5f88ef9f
poteatc <poteatc@outlook.com>
2024-12-24 13:21:53 -0500

    can click to highlight button for a second but need to fix highlighting pattern before and after button clicks

--- puzzle-games-frontend/color-memory-game/app.js
+++ puzzle-games-frontend/color-memory-game/app.js
@@ -14,13 +14,59 @@ function addToPattern() {
 let clicks = [];
 
 function setActive(button) {
-  button.classList.toggle("active");
+  button.classList.add("active");
 }
 
-buttons.forEach((button) => {
-  button.addEventListener("click", function (event) {
-    setActive(button);
-  });
-});
+function setInactive(button) {
+  button.classList.remove("active");
+}
 
-//let intervalId = setInterval(addToPattern, 1000);
+// buttons.forEach((button) => {
+//   button.addEventListener("click", function (event) {
+//     setActive(button);
+//   });
+// });
+
+addToPattern();
+
+function flash(index) {
+  buttons[index].classList.toggle("active");
+  //setInterval(flash(index), 1000);
+}
+
+addToPattern();
+
+//let flashIntervalId = setInterval(sequence, 1000);
+
+// function iterateAndClear(array, callback, interval) {
+//   let index = 0;
+//   const intervalId = setInterval(() => {
+//     if (index < array.length) {
+//       callback(array[index]);
+//       setTimeout(callback(array[index]), 1000);
+//       index++;
+//     } else {
+//       clearInterval(intervalId);
+//     }
+//   }, interval);
+// }
+
+// iterateAndClear(pattern, flash, 1000);
+
+const onClick = function () {
+  //console.log(this.id);
+  clicks.push(this.id);
+  this.classList.toggle("active");
+  setTimeout(() => {
+    this.classList.toggle("active");
+  }, 1000);
+  console.log(clicks);
+  //setTimeout(setInactive(this), 2000);
+  //   startInterval();
+  //   //setActive(this);
+  //   pauseInterval();
+};
+
+buttons.forEach((button) => {
+  button.onclick = onClick;
+});
